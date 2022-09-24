e86cab08b1d711a7dbcb47bc51630c74b9d09501
Punith1997 <punith1097@gmail.com>
2022-09-24 15:36:49 +0530

    customer dashboard

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,4 @@
+import simplejson as json
 from django.contrib.auth.decorators import login_required
 
 from accounts.forms import UserInfoForm, UserProfileForm
@@ -6,6 +7,8 @@ from accounts.models import UserProfile
 from django.shortcuts import redirect, get_object_or_404, render
 
 from django.contrib import messages
+
+from orders.models import Order, OrderedFood
 
 
 @login_required(login_url='login')
@@ -32,3 +35,29 @@ def cprofile(request):
         'profile': profile
     }
     return render(request, 'customers/cprofile.html', context)
+
+def my_orders(request):
+    orders = Order.objects.filter(user = request.user, is_ordered = True).order_by('-created_at')
+    
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'customers/my_orders.html', context)
+
+def order_detail(request, order_number = None):
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered = True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'ordered_food': ordered_food,
+            'order': order,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+        }
+        return render(request, "customers/order_detail.html", context)
+    except:
+        return redirect('customers')

35716b811131f4183ed1722a5e36989bbd9e9c30
Punith1997 <punith1097@gmail.com>
2022-09-29 18:25:07 +0530

    deploy to linode server

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.38.153', '127.0.0.1', 'djangofoodonline.tk', 'www.djangofoodonline.tk']
 
 
 # Application definition
